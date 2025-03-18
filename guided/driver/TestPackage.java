0ffa1b84c16865e6bbeee3c47e44e9d93de5d149
CandraDewi <agungcandra7788@gmail.com>
2025-03-18 21:54:27 +0700

    laprak 2

---
+++ guided/driver/TestPackage.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package driver;
+
+import hargabuku.KelasHarga;
+import hargatoken.KelasToken;
+
+/**
+ *
+ * @author REDMI
+ */
+
+public class TestPackage {
+    public static void main(String[] args){
+        
+        KelasToken token = new KelasToken();
+        KelasHarga harga = new KelasHarga();
+ 
+        token.info();
+        harga.info();
+    }
+}
