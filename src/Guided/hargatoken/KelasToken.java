0e4028ce189053993953b2c2940e940bbbd0ae3c
CandraDewi <87966244+CandraDewi@users.noreply.github.com>
2025-03-19 10:09:48 +0700

    Laprak 2 done

---
+++ src/Guided/hargatoken/KelasToken.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hargatoken;
+
+/**
+ *
+ * @author REDMI
+ */
+public class KelasToken {
+      public void info(){
+        System.out.println("Harga Buku");
+        System.out.println("Rp. 3000");
+        System.out.println("RP. 500");
+        System.out.println("Rp. 69");
+        System.out.println("Rp. 23");
+        System.out.println("Rp. 900000");
+        System.out.println("Hi this is from harga ");
+    }
+    
+}
